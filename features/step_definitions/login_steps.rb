84dfac7edb50bc19db8e34ab5422d5b12e47df99
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-05-30 05:28:32 +0200

    features/step_definitions/login_steps.rb

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,15 @@
+Given('that I visit the homepage') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I am asked to login with my nickname') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I login as {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I see the welcome message {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
