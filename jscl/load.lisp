a7026a161c63031f1b9b88217251dcc26533215f
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-06-03 10:33:38 +0200

    ch

---
+++ jscl/load.lisp
@@ -0,0 +1,17 @@
+(in-package :cl-user)
+
+(load "jscl.lisp")
+
+(defun compile-application ()
+  (jscl:compile-application
+   (list "../clog.lisp"
+         "../mini-fiveam.lisp"
+         "../utils.lisp"
+         "../othello.lisp"
+         "../m-macro.lisp"
+         "../myapp.lisp")
+   "../public/myapp.js"))
+
+(defun bootstrap-and-compile-application ()
+  (jscl:bootstrap)
+  (compile-application))
